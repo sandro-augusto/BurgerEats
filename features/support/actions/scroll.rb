556850d801ddf7e7125c20711a1148f32b902a73
Sandrinholeite10 <sandroaugusto10@gmail.com>
2023-01-27 22:43:51 -0300

    inclusão do move_to

--- features/support/actions/scroll.rb
+++ features/support/actions/scroll.rb
@@ -7,4 +7,13 @@ def scroll_to(el)
            .perform
 end
 
+def move_to(el)
+    wait_for_display(el)
+    move_to = driver.find_element(:"#{el['type']}", el['value'])
+    $driver.action
+    .move_to(move_to)
+    .perform
+end
+
+
 
