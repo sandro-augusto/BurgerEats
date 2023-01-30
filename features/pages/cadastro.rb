0bdb286ff72d9b2bdf69b4a31be5bb09daf6854d
la-barros <luizaugusto.evangelista@hotmail.com>
2023-01-30 15:36:56 -0300

    Alteração no cadastro

--- features/pages/cadastro.rb
+++ features/pages/cadastro.rb
@@ -19,62 +19,8 @@ class CadastroPage
         text_exists?('Voltar para home')
     end
 
-    def cadastro_uber_eats metodo_entrega
-        case metodo_entrega
-        when "Moto"
-            set(@map['name'], faker_name)
-            set(@map['cpf'], faker_cpf)
-            set(@map['email'], faker_email)
-            set(@map['whatsapp'], faker_number)
-            scroll_to(@map['numero_casa'])
-            set(@map['cep'], faker_cep)
-            click(@map['btn_cep'])
-            set(@map['numero_casa'], "10")
-            set(@map['complemento'], "Casa")
-            click(@map['metodo_moto'])
-            scroll_to(@map['upload'])
-            sleep 2
-            upload
-            sleep 1
-            click(@map['btn_cadastrar'])
-            sleep 1
-        when "Bicicleta"
-            set(@map['name'], faker_name)
-            set(@map['cpf'], faker_cpf)
-            set(@map['email'], faker_email)
-            set(@map['whatsapp'], faker_number)
-            scroll_to(@map['numero_casa'])
-            set(@map['cep'], faker_cep)
-            click(@map['btn_cep'])
-            set(@map['numero_casa'], "10")
-            set(@map['complemento'], "Casa")
-            click(@map['metodo_bicicleta'])
-            scroll_to(@map['upload'])
-            sleep 2
-            upload
-            sleep 1
-            click(@map['btn_cadastrar'])
-            sleep 1
-        when "Van_carro"
-            set(@map['name'], faker_name)
-            set(@map['cpf'], faker_cpf)
-            set(@map['email'], faker_email)
-            set(@map['whatsapp'], faker_number)
-            scroll_to(@map['numero_casa'])
-            set(@map['cep'], faker_cep)
-            click(@map['btn_cep'])
-            set(@map['numero_casa'], "10")
-            set(@map['complemento'], "Casa")
-            click(@map['metodo_van_carro'])
-            scroll_to(@map['upload'])
-            sleep 2
-            upload
-            sleep 1
-            click(@map['btn_cadastrar'])
-            sleep 1
-        else 
-            puts "Escolha uma opção valida!"
-        end
+    def cadastro_uber_eats
+        
     end
     
         
--- features/specs/cadastro.feature
+++ features/specs/cadastro.feature
@@ -12,13 +12,5 @@ Então é direcionado para pagina de cadastro
 
 
 @validacadastro
-Esquema do Cenário: Cadastro UberEats para Moto, Bicicleta e Van/Carro
-Dado que esteja na pagina de cadastro
-Quando preencher os campos necessários para o cadastro do "<metodo_entrega>"
-Então verifico a mensagem de sucesso 
 
-Exemplos:
-|metodo_entrega|
-|Moto          |
-|Bicicleta     |
-|Van_carro     |
+
